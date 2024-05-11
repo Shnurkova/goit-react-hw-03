a3169f42bab03ec0d0e968258547251559ba177c
Shnurkova <Annshnur@gmail.com>
2024-05-11 23:51:14 +0300

    add structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import initialContacts from '../../contacts.json'
+import './App.module.css'
+import ContactForm from '../ContactForm/ContactForm.jsx'
+import SearchBox from '../SearchBox/SearchBox.jsx'
+import ContactList from '../ContactList/ContactList.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+      </>
+  )
+}
+
+export default App;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,7 @@
+export default function ContactList() {
+    <>
+        return (
+        <span>Hello</span>
+        )
+    </>
+}
