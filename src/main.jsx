514fce83f75da0564c628cc69293ac4e484ff303
Shnurkova <Annshnur@gmail.com>
2024-05-12 16:45:48 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './componentsqe/App'
+import App from './components/App.jsx'
 import './index.css'
 import "modern-normalize";
 
